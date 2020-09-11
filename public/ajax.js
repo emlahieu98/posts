fff7cfdb6fe0c96361b4e2e6d19f34e6ab73830f
emlahieu98 <hieucr98tb@gmail.com>
2020-09-11 11:55:38 +0700

    add update post

--- public/ajax.js
+++ public/ajax.js
@@ -1,73 +1,70 @@
 $(document).ready(function () {
-    $('.summernote-new').summernote({
-      placeholder: 'Writing.....',
-      tabsize: 2,
-      height: 300,
-      toolbar: [
-        ['style', ['style']],
-        ['font', ['bold', 'underline', 'clear']],
-        ['color', ['color']],
-        ['para', ['ul', 'ol', 'paragraph']],
-        ['table', ['table']],
-        // ['insert', ['link', 'picture', 'video']],
-        ['view', ['fullscreen', 'codeview', 'help']]
-      ]
-    });
-    $('#summernote-edit').summernote({
-      placeholder: 'Writing.....',
-      tabsize: 2,
-      height: 300,
-      toolbar: [
-        ['style', ['style']],
-        ['font', ['bold', 'underline', 'clear']],
-        ['color', ['color']],
-        ['para', ['ul', 'ol', 'paragraph']],
-        ['table', ['table']],
-        // ['insert', ['link', 'picture', 'video']],
-        ['view', ['fullscreen', 'codeview', 'help']]
-      ]
-    });
-    $("#form-add-post").validate({
-      rules: {
-        title: { required: true },
-        description: { required: true },
-        content: { required: true },
-      },
-      messages: {
-        title: { required: " Title must not empty" },
-        description: {
-          required: " Description must not empty",
-        },
-        content: { required: " Content must not empty" },
-      },
-    });
+  $(".summernote-new").summernote({
+    placeholder: "Writing.....",
+    tabsize: 2,
+    height: 300,
+    toolbar: [
+      ["style", ["style"]],
+      ["font", ["bold", "underline", "clear"]],
+      ["color", ["color"]],
+      ["para", ["ul", "ol", "paragraph"]],
+      ["table", ["table"]],
+      // ['insert', ['link', 'picture', 'video']],
+      ["view", ["fullscreen", "codeview", "help"]],
+    ],
+  });
+  $("#summernote-edit").summernote({
+    placeholder: "Writing.....",
+    tabsize: 2,
+    height: 300,
+    toolbar: [
+      ["style", ["style"]],
+      ["font", ["bold", "underline", "clear"]],
+      ["color", ["color"]],
+      ["para", ["ul", "ol", "paragraph"]],
+      ["table", ["table"]],
+      // ['insert', ['link', 'picture', 'video']],
+      ["view", ["fullscreen", "codeview", "help"]],
+    ],
+  });
   $("#btn-add-post").click(function () {
-      
-      var title = $("#title").val();
-      var description = $("#description").val();
-      var content = $(".summernote-new").summernote("code");
-    var fullName = $("#fullName").val();
-    console.log(description)
-      if ($("#form-add-post").valid()) {
-        $.ajax({
-          url: "/admin/posts/add",
-          method: "POST",
-          data: {
-            title: title,
-            description: description,
-            content: content,
-            
-          },
-        })
-          .then(function (data) {
-            alertify.success(data.message);
-            window.timeout = setTimeout(function () {
-              window.location.href = "/admin/posts";
-            }, 2000);
-          })
-          .catch(function (err) {
-            alertify.error(err.responseJSON.message);
-          });
-      }
-    });
+    var title = $("#title").val();
+    var description = $("#description").val();
+    let content = $(".summernote-new").summernote("code");
+    $("#content").val(content);
+    if (title != '' && description != '' && content != '') {
+      alert("Create new post success");
+    }
+  }
+  );
+
+  //   $("#btn-add-post").click(function () {
+
+  //       var title = $("#title").val();
+  //       var description = $("#description").val();
+  //     var content = $(".summernote-new").summernote("code");
+  //     console.log(content);
+  //  //   var fullName = $("#fullName").val();
+  //    //   if ($("#form-add-post").valid()) {
+  //         $.ajax({
+  //           url: "/admin/posts/add",
+  //           method: "POST",
+  //           data: {
+  //             title: title,
+  //             description: description,
+  //             content: content,
+
+  //           },
+  //         })
+  //           .then(function (data) {
+  //             alertify.success(data.message);
+  //             window.timeout = setTimeout(function () {
+  //               window.location.href = "/admin/posts";
+  //             }, 2000);
+  //           })
+  //           .catch(function (err) {
+  //             alertify.error(err.responseJSON.message);
+  //           });
+  //   //    }
+  //     });
 });
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,80 +3,71 @@ const router = express.Router();
 const { post } = require("./../models/function");
 const { post_alert } = require("../err/en");
 const multer = require("multer");
-
 const upload = multer({
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "/tmp");
+      cb(null, "./public/uploads/");
     },
     filename: function (req, file, cb) {
-      cb(null, file.fieldname + "-" + Date.now());
+      cb(null, Date.now() + "-" +file.originalname);
     },
   }),
 });
-
 //get all posts
 router.get("/",async(req, res) => {
   const posts = await post.set({}, "");
   return res.render("admin/index", {posts});
 });
 //get 1 posts
-router.post("/add", async (req, res) => {
+router.post("/add", upload.array("file-new"), async (req, res) => {
   try {
-    return res.render("index", { data });
-    const post = await post.create({
+    const files = req.files;
+    const newPost = {
       title: req.body.title,
-    //  image: req.file.filename,
-      content: req.body.content,
       description: req.body.description,
-      image: image.push(req.file.filename),
+      image: [],
+      content: req.body.content,
+    }
+    files.map((img, index) => {
+      posts.image.push(img.filename)
     })
-    return res.status(200).json({
-      status: "success",
-      message: post_alert.post_create,
-    });
-
+    await post.create(newPost);
+    return res.redirect("/admin/posts")
   } catch (error) {
-    return res.status(400).json({
-      status: "fail",
-      message: post_alert.err_server,
-    });
+    return res.render("/admin/posts")
   }
 });
-// //post 1 posts
-// router.post("/add",  async (req, res) => {
-//   try {
-//   await upload(req, res, function (err) {
-//     if (err instanceof multer.MulterError) {
-//       console.log("A Multer error occurred when uploading.");
-//     } else if (err) {
-//       console.log("An unknown error occurred when uploading." + err);
-//     }else {
-//         const data = post.create({
-//           title: req.body.title,
-//           image: req.file.filename,
-//           content: req.body.content,
-//           description: req.body.description,
-//         }); 
-//       return res.redirect("/posts");
-//       }
-//     });
-//   } catch (error) {
-//     res.json(error)
-//   }
-// })
-// //get id post
-// router.get("/:id", async (req, res) => {
-//   const { id } = req.params;
-//   res.json(await post.get({ _id: id }, ""));
-// });
-// //update id post
-// router.put("/:id", async (req, res) => {
-//   res.json(await post.set({ _id: req.params.id }, req.body))
-// });
+
+//get id post
+router.get("/edit/:id", async (req, res) => {
+   const { id } = req.params;
+  const posts = await post.get({ _id: id }, "");
+  return res.render("/admin/posts", {posts});
+});
+//update id post
+router.post("/edit/:id", upload.array("file-new"), async (req, res) => {
+  try {
+    const { id } = req.params;
+    const files = req.files;
+    const updatePost = {
+      title: req.body.title,
+      description: req.body.description,
+      image: [],
+      content: req.body.content,
+    };
+    files.map((img, index) => {
+      posts.image.push(img.filename);
+    });
+    await post.set({ _id: id }, updatePost);
+    return res.redirect("/admin/posts");
+  } catch (error) {
+    return res.render("/admin/posts");
+  }
+});
 // //delete id post
 // router.delete("/:id", async (req, res) => {
 //   res.json(await post.del({ _id: req.params.id }));
 // });
 
 module.exports = router;
+//   res.json(await post.set({ _id: id }, req.body));
